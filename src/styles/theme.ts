7daf1bfdee33e6f10a5ee8530a1030ea59a5960e
Jorge Hecherat <hecherat@gmail.com>
2020-10-01 18:42:58 -0300

    [develop] - fonts added to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,26 +1,68 @@
 import { DefaultTheme } from 'styled-components'
 
-// darkPurple: '#232129',
-// gray1: '#666360',
-// red: '#C53030',
 export const themeColors = {
   background: '#232F45',
-  gray1: '#9197A2',
+  gray1: '#EBEBEB',
+  gray2: '#BABABA',
+  gray3: '#898989',
+  gray4: '#272727',
   magenta1: '#E53B6A',
   magenta2: '#E62254',
   white1: '#ffffff',
   white2: '#f4ede8',
 }
 
+const genericFontType = {
+  'font-family': 'Roboto',
+}
+
+export const fontTypes = {
+  heading: {
+    ...genericFontType,
+    fontSize: 18,
+    letterSpacing: 0,
+    lineHeight: '25px',
+  },
+  title: {
+    fontSize: 14,
+    letterSpacing: 0.25,
+    fontWeight: 600,
+    lineHeight: '19px',
+  },
+  subheading: {
+    ...genericFontType,
+    fontSize: 11,
+    letterSpacing: 1.5,
+    fontWeight: 600,
+    lineHeight: '15px',
+  },
+  body: {
+    ...genericFontType,
+    'font-size': 14,
+    letterSpacing: 0,
+    lineHeight: '19px',
+  },
+  button: {
+    'font-size': 11,
+    fontWeight: 600,
+    letterSpacing: 1.5,
+    textTransform: 'uppercase',
+    lineHeight: '15px',
+  },
+}
+
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: { [key in keyof typeof themeColors]: string }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    fontTypes: { [key in keyof typeof fontTypes]: any }
     spacing: (x: number) => number
   }
 }
 
 const theme: DefaultTheme = {
   colors: themeColors,
+  fontTypes,
   spacing: number => number * 8,
 }
 

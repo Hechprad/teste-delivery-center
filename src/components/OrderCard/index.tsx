cce985a91adb253962a08a414e56a90378dea50c
Jorge Hecherat <hecherat@gmail.com>
2020-10-01 22:38:05 -0300

    [develop] - update OrderCard component

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { v4 } from 'uuid'
 
 import { formatMoney, pendingAmount } from 'utils'
 import { IOrder } from 'graphql/types'
@@ -8,29 +9,39 @@ import * as s from './styles'
 const OrderCard: React.FC<{ order: IOrder }> = ({
   order: { reference, store, customer, amount, deliveryFee, payments },
   ...rest
-}) => (
-  <s.Wrapper {...rest}>
-    <s.HorizontalItem>
-      <s.ItemTitle>Id do pedido:</s.ItemTitle>
-      <s.Text>{reference}</s.Text>
-    </s.HorizontalItem>
-    <s.HorizontalItem>
-      <s.ItemTitle>Nome do lojista:</s.ItemTitle>
-      <s.Text>{store}</s.Text>
-    </s.HorizontalItem>
-    <s.HorizontalItem>
-      <s.ItemTitle>Nome do cliente:</s.ItemTitle>
-      <s.Text>{customer.name}</s.Text>
-    </s.HorizontalItem>
-    <s.HorizontalItem>
-      <s.ItemTitle>Total (Frete incluso):</s.ItemTitle>
-      <s.Text>{formatMoney(amount + deliveryFee)}</s.Text>
-    </s.HorizontalItem>
-    <s.HorizontalItem>
-      <s.ItemTitle>Valor pendente:</s.ItemTitle>
-      <s.Text>{pendingAmount(amount, deliveryFee, payments)}</s.Text>
-    </s.HorizontalItem>
-  </s.Wrapper>
-)
+}) => {
+  const content = [
+    {
+      title: 'Id do pedido:',
+      fieldValue: reference,
+    },
+    {
+      title: 'Nome do lojista:',
+      fieldValue: store,
+    },
+    {
+      title: 'Nome do cliente:',
+      fieldValue: customer.name,
+    },
+    {
+      title: 'Total (Frete incluso):',
+      fieldValue: formatMoney(amount + deliveryFee),
+    },
+    {
+      title: 'Valor pendente:',
+      fieldValue: pendingAmount(amount, deliveryFee, payments),
+    },
+  ]
+  return (
+    <s.Wrapper {...rest}>
+      {content.map(lineContent => (
+        <s.HorizontalItem key={v4()}>
+          <s.ItemTitle>{lineContent.title}</s.ItemTitle>
+          <s.Text>{lineContent.fieldValue}</s.Text>
+        </s.HorizontalItem>
+      ))}
+    </s.Wrapper>
+  )
+}
 
 export default OrderCard

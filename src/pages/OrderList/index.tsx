de3c23f673d055846aeccc4e352eb3a5a540dcaf
Jorge Hecherat <hecherat@gmail.com>
2020-10-01 21:53:38 -0300

    [develop] - add OrderList component

---
+++ src/pages/OrderList/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { useLazyQuery } from '@apollo/react-hooks'
+import { useHistory } from 'react-router-dom'
+import { v4 } from 'uuid'
+
+import OrderCard from 'components/OrderCard'
+
+import GET_ORDERS from 'graphql/queries/getOrders'
+import { IOrder } from 'graphql/types'
+
+import * as s from './styles'
+
+const OrderList: React.FC = () => {
+  const history = useHistory()
+
+  const [getOrders, { loading, error, data }] = useLazyQuery(GET_ORDERS)
+  const [ordersList, setOrdersList] = useState<IOrder[] | null>()
+
+  const handleRedirectToDetails = (reference: number): void => {
+    history.push(`/pedido/${reference}`)
+  }
+
+  useEffect(() => {
+    getOrders()
+  }, [getOrders])
+
+  useEffect(() => {
+    if (data) {
+      setOrdersList(data.orders)
+    }
+  }, [data])
+
+  const displayContent = (): React.ReactNode => {
+    if (error && !loading) {
+      return <s.Button onClick={() => getOrders()}>tente novamente</s.Button>
+    }
+
+    if (!error && loading) {
+      return <s.Loading>Carregando...</s.Loading>
+    }
+
+    if (ordersList && ordersList.length > 0) {
+      return (
+        <ul>
+          {ordersList?.map(order => (
+            <s.Li
+              key={v4()}
+              onClick={() => handleRedirectToDetails(order.reference)}
+            >
+              <OrderCard order={order} />
+            </s.Li>
+          ))}
+        </ul>
+      )
+    }
+
+    return <s.Loading>Carregando...</s.Loading>
+  }
+
+  return (
+    <s.Section>
+      <s.PageTitle>Lista de Pedidos</s.PageTitle>
+      {displayContent()}
+    </s.Section>
+  )
+}
+
+export default OrderList
---
+++ src/pages/OrderList/styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import MaterialUIButton from '@material-ui/core/Button'
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`
+
+export const Li = styled.li`
+  background-color: ${({ theme }) => theme.colors.white2};
+  justify-content: center;
+  width: 300px;
+  height: 100%;
+  padding: ${({ theme }) => `${theme.spacing(2)}px`};
+  border-radius: 4px;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.gray1};
+  }
+
+  :active {
+    background-color: ${({ theme }) => theme.colors.gray3};
+  }
+
+  & + & {
+    margin-top: ${({ theme }) => `${theme.spacing(2)}px`};
+  }
+`
+
+export const PageTitle = styled.h3`
+  ${({ theme }) => theme.fontTypes.heading}
+  color: ${({ theme }) => theme.colors.white2};
+  margin: ${({ theme }) => `${theme.spacing(2)}px 0 ${theme.spacing(5)}px`};
+`
+
+export const Loading = styled.p`
+  ${({ theme }) => theme.fontTypes.heading};
+  color: ${({ theme }) => theme.colors.white2};
+  margin-top: ${({ theme }) => `${theme.spacing(2)}px`};
+`
+
+export const Button = styled(MaterialUIButton).attrs(() => ({
+  type: 'button',
+  variant: 'contained',
+  color: 'primary',
+}))``
